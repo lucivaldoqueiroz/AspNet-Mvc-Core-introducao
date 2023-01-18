3db9b400c0e0a6d2e1af22e5cd4ef159388df21e
Lucivaldo Queiroz <lucivaldo.npqueiroz@gmail.com>
2023-01-18 10:20:15 -0300

    Projeto inicial MVC dotnet 6.0

---
+++ AppModelo/src/lnpqdev.iu.AppModelo/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lnpqdev.iu.AppModelo.Controllers
+{
+	public class HomeController: Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ AppModelo/src/lnpqdev.iu.AppModelo/Program.cs
@@ -0,0 +1,22 @@
+// Tudo inicia a partir do builder
+var builder = WebApplication.CreateBuilder(args);
+
+// Adicionando o MVC ao container
+builder.Services.AddControllersWithViews();
+
+// Realizando o buid das configurações que resultará na App
+var app = builder.Build();
+
+// Ativando a pagina de erros caso seja ambiente de desenvolvimento
+if (app.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();
+}
+
+// Adicionando Rota padrão
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Colocando a App para rodar
+app.Run();
